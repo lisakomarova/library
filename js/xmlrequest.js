3e891d0359950a3bffad50026d67f891fa692b96
lisakomarova <lisakomarova20000@gmail.comgit config --global user.email lisakomarova2000@gmail.comclear>
2018-12-16 20:41:50 +0300

    gfd

--- js/xmlrequest.js
+++ js/xmlrequest.js
@@ -90,3 +90,72 @@ $(document).ready(function(){
         }        
     })
 });
+$(document).ready(function(){
+    var table = "<table>";
+    $.ajax({
+        type:"GET",
+        url:"../xml/russian.xml",
+        dataType:"xml",
+        success:function(xml){
+
+            var events = xml.children[0].children;
+
+            $.each(events, function(){
+                var event = $(this);
+                var name=event.find("AUTHOR").text();
+                var date=event.find("TITLE").text();
+                var place=event.find("LINK").text();
+                table +='<tr><td> <a href = "' + place + '">'+ name +' - '+ date +'</a></td></tr>';
+            })    
+
+            table += "</table>"
+            $('.russian').append(table);
+        }        
+    })
+});
+$(document).ready(function(){
+    var table = "<table>";
+    $.ajax({
+        type:"GET",
+        url:"../xml/english.xml",
+        dataType:"xml",
+        success:function(xml){
+
+            var events = xml.children[0].children;
+
+            $.each(events, function(){
+                var event = $(this);
+                var name=event.find("AUTHOR").text();
+                var date=event.find("TITLE").text();
+                var place=event.find("LINK").text();
+                table +='<tr><td> <a href = "' + place + '">'+ name +' - '+ date +'</a></td></tr>';
+            })    
+
+            table += "</table>"
+            $('.english').append(table);
+        }        
+    })
+});
+$(document).ready(function(){
+    var table = "<table>";
+    $.ajax({
+        type:"GET",
+        url:"../xml/bstu.xml",
+        dataType:"xml",
+        success:function(xml){
+
+            var events = xml.children[0].children;
+
+            $.each(events, function(){
+                var event = $(this);
+                var name=event.find("AUTHOR").text();
+                var date=event.find("TITLE").text();
+                var place=event.find("LINK").text();
+                table +='<tr><td> <a href = "' + place + '">'+ name +' - '+ date +'</a></td></tr>';
+            })    
+
+            table += "</table>"
+            $('.bstu').append(table);
+        }        
+    })
+});
